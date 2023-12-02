40d63ac9b779e15f8f6a7b6704108f358510f048
olatz <gotiolatz@gmail.com>
2023-12-02 13:37:07 +0100

    endpoints

--- README.md
+++ README.md
@@ -2,18 +2,19 @@
 
 1 Creación de main.js para la creación de bases de datos desde el vscode utilizando express.
 
--Importo express y mysql2 y creo puerto de conexión al localhost
--Creación de conexión a la bbdd de SQL
+- Importo express y mysql2 y creo puerto de conexión al localhost
+- Creación de conexión a la bbdd de SQL
 
--Creación de endpoint para la creación de base de datos, tablas e inserción de productos y categorias utilizando:
-    - app.get
-    - app.post
+- Creación de endpoints para:
+ - creación de base de datos, tablas y relaciones N:M
+ - inserción de productos y categorias
+ - getAll (products y categories)
+ - getAll con relación INNER JOIN
 
 
 
   Ejercicio 4
-Crea un endpoint  que muestre todos los productos(getAllProducts)
-Crea un endpoint que muestre todas las categorías(getAllCategories)
+
 Crea un endpoint que muestra todos los productos con sus categorías 
 Crea un endpoint donde puedas seleccionar un producto por id
 Crea un endpoint que muestre de forma descendente los productos.
--- main.js
+++ main.js
@@ -49,22 +49,64 @@ app.get('/createTableCategories', (req, res) => {
 	})
 })
 
+app.get('/createtableproductscategories', (req,res) =>{
+const sql= 'CREATE TABLE productsCategories( id_pro INT, id_cat INT, FOREIGN KEY(id_pro) REFERENCES products(id), FOREIGN KEY(id_cat) REFERENCES categories(id))';
 
-const sql= 'CREATE TABLE productsCategories(id_pro int, id_cat int)'
+
+		db.query(sql, (err,result)=>{
+
+		if(err) throw err
+		console.log(result);
+		res.send('Productscategories table created...')
+	})
+})
+
+/*
+
+
+CREATE TABLE comments(
+	id INT AUTO_INCREMENT,
+	post_id INT,
+	user_id INT,
+	body TEXT,
+	PRIMARY KEY(id),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(post_id) REFERENCES posts(id)
+	);
+	
+*/
 
 
 
 //EJERCICIO 3
-app.post('/newproduct', (req,res)=>{
+app.post('/newproduct', (req,res)=>
+{
+const result = req.body;
+	result.forEach((data) => {
 
-    const sql = `INSERT INTO products(name, price) VALUES('${req.body.name}', '${req.body.price}')`
-console.log(sql);
-    db.query(sql,(err, result)=>{
-        if(err) throw err
-        console.log(result)
-        res.send('Products inserted')
-    })
+	const sql = 'INSERT INTO products(name, price) VALUES(?, ?)';
+
+	const values = [data.name, data.price];
+	console.log(sql);
+		
+		db.query(sql, values, (error, result) => {
+		if(error) throw error;
+			console.log('data inserted',result);
+			res.send('Products inserted');
+		})
 })
+})
+/*
+dataArray.forEach((data) => {
+	const sql = 'INSERT INTO your_table (field1, field2) VALUES (?, ?)';
+	const values = [data.field1, data.field2];
+  
+	connection.query(sql, values, (error, results, fields) => {
+	  if (error) throw error;
+	  console.log('Data inserted:', results);
+	});
+  });
+*/
 
 app.post('/newcategory', (req,res)=>{
 
@@ -76,6 +118,9 @@ app.post('/newcategory', (req,res)=>{
         res.send('category inserted')
     })
 })
+
+
+//EJERCICIO 4
 
 app.get('/getAllProducts', (req,res)=>{
 const sql = 'SELECT * FROM products';
@@ -97,8 +142,54 @@ app.get('/getAllCategories', (req,res)=>{
 	)
 })
 
+//Crea un endpoint que muestre de forma descendente los productos.
+//Crea un endpoint donde puedas buscar un producto por su nombre
 
 
+//Crea un endpoint que muestra todos los productos con sus categorías 
+app.get('/getAllprodcat', (req,res)=>{
+	
+	const sql = 'SELECT products.name, products.price, categories.name from products INNER JOIN productsCategories ON products.id = productsCategories.id_pro inner join categories ON productsCategories.id_cat = categories.id';
+	db.query(sql, (err, result)=>{
+		
+		if(err)throw err
+		res.send(result);
+	})
+	
+})
+
+//Crea un endpoint donde puedas seleccionar un producto por id
+
+app.post('/getProdById', (req,res)=>{
+	
+	const sql = `SELECT name, price FROM products where id = ${req.body.id}`;
+	
+	db.query(sql,(err,result)=>{
+		
+		if(err)throw err
+		res.send(result);
+	})
+	
+})
+
+//Crea un endpoint donde puedas seleccionar una categoría por id
+app.post('/getCatById', (req,res)=>{
+
+	const sql = `SELECT name FROM categories where id = ${req.body.id}`;
+
+	db.query(sql, (err,result)=>{
+
+		if(err) throw err
+		res.send(result);
+	})
+})
+
+
+/*
+sELECT comments.body, posts.title, users.first_name, users.last_name FROM comments
+INNER JOIN posts ON posts.id = comments.post_id
+INNER JOIN users ON users.id = comments.user_id;
+*/
 
 
 
